89c6326c5b578ce7629d2066134e56d6928e3d6b
aemunozv <albertoemunozv@gmail.com>
2022-09-19 20:19:27 -0500

    first commit

---
+++ nosotros.php
@@ -0,0 +1,12 @@
+<?php include("template/cabecera.php");?>
+   
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola Somos un grupo de personas que se dedican al Desarrollo Web</p>
+    <hr class="my-2">
+</div>
+
+
+<?php include("template/pie.php");?>
+
